89e309b103b412ae4c21948465885b0d727adcc2
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-03-04 13:44:24 +0100

    add db connectiontion

--- Platform-Movies/Program.cs
+++ Platform-Movies/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Platform_Movies.Models.Domain;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DatabaseContext>(opcoes =>
+{
+    opcoes.UseSqlServer(builder.Configuration.GetConnectionString("Mycon"));
+});
 
 var app = builder.Build();
 
