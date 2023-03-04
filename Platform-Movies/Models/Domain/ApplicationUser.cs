2b25c856b5d2e845007ee7c81fb76ceb78eeef46
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-03-04 13:25:27 +0100

    add classes of Domain folder

---
+++ Platform-Movies/Models/Domain/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Platform_Movies.Models.Domain
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string Name { get; set; }
+
+    }
+}
---
+++ Platform-Movies/Models/Domain/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Platform_Movies.Models.Domain
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? GenreName { get; set; }
+    }
+}
