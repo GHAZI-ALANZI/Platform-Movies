e24cd30996aa781b0026fc22c735e9ac34f6552f
GHAZI ALANZI <105205339+GHAZI-ALANZI@users.noreply.github.com>
2023-03-08 12:15:14 +0100

    add Genre and Auth controllert

---
+++ Platform-Movies/Controllers/GenreController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Platform_Movies.Controllers
+{
+    public class GenreController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
